a4339c8668cc0a3f6b80d17dd8431dbb728be1a2
cristianomatos <cristianobmatos@gmail.com>
2017-09-05 13:26:07 +0800

    RecentSettings: recents panel clear all button tweaks
    * Show or hide recents button
    * Place the button in top left, center, right or bottom left, center, right

--- src/com/nest/settings/ui/RecentsSettings.java
+++ src/com/nest/settings/ui/RecentsSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Invictrix ROM
+ * Copyright (C) 2018 Scorpion
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,26 +16,65 @@
 
 package com.nest.settings.ui;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
-
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
 
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
 
+public class RecentsSettings extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
 
-public class RecentsSettings extends ScorpionSettingsFragment implements OnPreferenceChangeListener {
-    
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+    private ListPreference mRecentsClearAllLocation;
+    private SwitchPreference mRecentsClearAll;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.settings_recents);
- }
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
 
- @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return true;
+        addPreferencesFromResource(R.xml.settings_recents);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        // clear all recents
+        mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
+        int location = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
+        mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
+        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mRecentsClearAllLocation) {
+            int location = Integer.valueOf((String) objValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+        return true;
+        }
+    return false;
+
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SCORPION;
+    }
 }
