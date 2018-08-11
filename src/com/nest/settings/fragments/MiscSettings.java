0d5e54ab1a9b91436acb30cee11d178b8f7fa30b
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-11 02:40:33 -0700

    Smart Pixels [2/2]
    Change-Id: I55775e02e849ad5db5b1187be0ca77b7c09f1081

--- src/com/nest/settings/fragments/MiscSettings.java
+++ src/com/nest/settings/fragments/MiscSettings.java
@@ -47,11 +47,25 @@ import com.android.settings.SettingsPreferenceFragment;
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String SMART_PIXELS = "smart_pixels";
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.settings_misc);
+        updateSmartPixelsPreference();
+    }
+
+    private void updateSmartPixelsPreference() {
+        PreferenceScreen prefSet = getPreferenceScreen();
+        boolean enableSmartPixels = getContext().getResources().
+                getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
+        Preference smartPixels = findPreference(SMART_PIXELS);
+
+        if (!enableSmartPixels){
+            prefSet.removePreference(smartPixels);
+        }
 
     }
 
