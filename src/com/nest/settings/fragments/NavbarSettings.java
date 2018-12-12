2d4b5a32d8e3e8f6c9a15b425adf7e48efde65f9
Alex Cruz <alex@dirtyunicorns.com>
2018-12-11 23:32:27 -0600

    Move buttons to Dirty Tweaks [2/2]
    - Move everything into one fragment
    - Hide buttons if no hw keys config
    - Move navigation bar switch to navigation category
    - Make everything play nice with each other
    
    Change-Id: I01713979f9466d50c4ade3658e95c5b94efb891b

--- src/com/nest/settings/fragments/NavbarSettings.java
+++ src/com/nest/settings/fragments/NavbarSettings.java
@@ -40,10 +40,16 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 
-public class NavbarSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+public class NavigationBar extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
     private static final String KEY_PULSE_SETTINGS = "pulse_settings";
+    private static final String KEY_STOCK_NAVBAR = "stock_navbar";
+    private static final String KEY_NAVIGATION_BAR_ENABLED = "navigation_bar";
 
     private PreferenceScreen mPulseSettings;
+    private Preference mStockNavbar;
+    private SwitchPreference mNavigationBar;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -51,15 +57,46 @@ public class NavbarSettings extends SettingsPreferenceFragment implements Prefer
         addPreferencesFromResource(R.xml.settings_navigation);
 
         mPulseSettings = (PreferenceScreen) findPreference(KEY_PULSE_SETTINGS);
+
+        mStockNavbar = (Preference) findPreference(KEY_STOCK_NAVBAR);
+
+        mNavigationBar = (SwitchPreference) findPreference(KEY_NAVIGATION_BAR_ENABLED);
+        final boolean defaultToNavigationBar = getResources().getBoolean(
+                com.android.internal.R.bool.config_defaultToNavigationBar);
+        mNavigationBar.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_ENABLED,
+                defaultToNavigationBar ? 1 : 0) == 1));
+        mNavigationBar.setOnPreferenceChangeListener(this);
+
+        updateNavbar();
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mNavigationBar) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_ENABLED, value ? 1 : 0);
+            return true;
+        }
         return false;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SCORPION;
+    }
+
+    private void updateNavbar() {
+        boolean swipeUpGesture = Settings.Secure.getInt(getActivity().getContentResolver(),
+                Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0) != 0;
+        if (!swipeUpGesture) {
+            mStockNavbar.setEnabled(true);
+            mNavigationBar.setEnabled(true);
+            mStockNavbar.setSummary(R.string.systemui_tuner_navbar_enabled_summary);
+        } else {
+            mStockNavbar.setEnabled(false);
+            mNavigationBar.setEnabled(true);
+            mStockNavbar.setSummary(R.string.systemui_tuner_navbar_disabled_summary);
+        }
     }
 }
