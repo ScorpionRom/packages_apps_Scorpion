mike2587/abc_packages_apps_AbcSettings

Remove weather preference if package is some how removed/uninstalled
Some users tend to want to 'debloat' ROMs and sometimes are not the
sharpest tool in the shed and remove things that they're not suppose to.

So in the event that a user gets the "bright" idea of removing the weather
apk, we'll remove the preference. This way we're protected from users that
want to report bogus bugs (because they removed the apk).

Change-Id: I47987ec7793236a702fc5328e668fe65eb7484ee

src/com/abc/settings/LockScreenSettings.java
@@ -26,8 +26,12 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.abc.support.preferences.SystemSettingListPreference;
+
 public class LockScreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_WEATHER_TEMP = "weather_lockscreen_unit";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,12 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.abc_lockscreen_settings);
         ContentResolver resolver = getActivity().getContentResolver();
 
+        SystemSettingListPreference mWeatherTemp =
+                (SystemSettingListPreference) findPreference(KEY_WEATHER_TEMP);
+        if (!com.android.internal.util.du.Utils.isPackageInstalled(
+                getActivity(), "org.pixelexperience.weather.client")) {
+            getPreferenceScreen().removePreference(mWeatherTemp);
+        }
     }
 
     @Override

ScorpionRom/packages_apps_Scorpion

src/com/nest/settings/fragments/LockScreenSettings.java
@@ -35,6 +35,7 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 
+import com.nest.settings.preferences.SystemSettingListPreference;
 import com.nest.settings.preferences.Utils;
 
 import android.provider.Settings;
@@ -46,6 +47,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_FACE_AUTO_UNLOCK = "face_auto_unlock";
     private static final String KEY_FACE_UNLOCK_PACKAGE = "com.android.facelock";
+    private static final String KEY_WEATHER_TEMP = "weather_lockscreen_unit";
 
     private SwitchPreference mFaceUnlock;
 
@@ -65,6 +67,13 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
             mFaceUnlock.setChecked((Settings.Secure.getInt(getContext().getContentResolver(),
                     Settings.System.FACE_AUTO_UNLOCK, 0) == 1));
             mFaceUnlock.setOnPreferenceChangeListener(this);
+        }
+
+        SystemSettingListPreference mWeatherTemp =
+                (SystemSettingListPreference) findPreference(KEY_WEATHER_TEMP);
+        if (!com.android.internal.util.du.Utils.isPackageInstalled(
+                getActivity(), "org.pixelexperience.weather.client")) {
+            getPreferenceScreen().removePreference(mWeatherTemp);
         }
 
     }