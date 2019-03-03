DirtyUnicorns/android_packages_apps_DU-Tweaks

Navbar: Add custom app actions [2/3]
Change-Id: I96e83eae758d82136ab135e05793cc262e42d0b0

src/com/dirtyunicorns/tweaks/fragments/navbar/AppSwitchDoubleTap.java
@@ -1,0 +1,25 @@
+package com.dirtyunicorns.tweaks.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.dirtyunicorns.support.preferences.AppPicker;
+
+public class AppSwitchDoubleTap extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_APP_SWITCH_DOUBLE_TAP_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_APP_SWITCH_DOUBLE_TAP_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/dirtyunicorns/tweaks/fragments/navbar/AppSwitchLongPress.java
@@ -1,0 +1,25 @@
+package com.dirtyunicorns.tweaks.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.dirtyunicorns.support.preferences.AppPicker;
+
+public class AppSwitchLongPress extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_APP_SWITCH_LONG_PRESS_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_APP_SWITCH_LONG_PRESS_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/dirtyunicorns/tweaks/fragments/navbar/BackDoubleTap.java
@@ -1,0 +1,25 @@
+package com.dirtyunicorns.tweaks.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.dirtyunicorns.support.preferences.AppPicker;
+
+public class BackDoubleTap extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_DOUBLE_TAP_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_DOUBLE_TAP_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/dirtyunicorns/tweaks/fragments/navbar/BackLongPress.java
@@ -1,0 +1,25 @@
+package com.dirtyunicorns.tweaks.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.dirtyunicorns.support.preferences.AppPicker;
+
+public class BackLongPress extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_LONG_PRESS_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_LONG_PRESS_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/dirtyunicorns/tweaks/fragments/navbar/HomeDoubleTap.java
@@ -1,0 +1,25 @@
+package com.dirtyunicorns.tweaks.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.dirtyunicorns.support.preferences.AppPicker;
+
+public class HomeDoubleTap extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_HOME_DOUBLE_TAP_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_HOME_DOUBLE_TAP_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/dirtyunicorns/tweaks/fragments/navbar/HomeLongPress.java
@@ -1,0 +1,25 @@
+package com.dirtyunicorns.tweaks.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.dirtyunicorns.support.preferences.AppPicker;
+
+public class HomeLongPress extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_HOME_LONG_PRESS_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_HOME_LONG_PRESS_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

ScorpionRom/packages_apps_Scorpion

src/com/nest/settings/fragments/navbar/AppSwitchDoubleTap.java
@@ -1,0 +1,25 @@
+package com.nest.settings.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.nest.settings.preferences.AppPicker;
+
+public class AppSwitchDoubleTap extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_APP_SWITCH_DOUBLE_TAP_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_APP_SWITCH_DOUBLE_TAP_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/nest/settings/fragments/navbar/AppSwitchLongPress.java
@@ -1,0 +1,25 @@
+package com.nest.settings.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.nest.settings.preferences.AppPicker;
+
+public class AppSwitchLongPress extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_APP_SWITCH_LONG_PRESS_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_APP_SWITCH_LONG_PRESS_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/nest/settings/fragments/navbar/BackDoubleTap.java
@@ -1,0 +1,25 @@
+package com.nest.settings.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.nest.settings.preferences.AppPicker;
+
+public class BackDoubleTap extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_DOUBLE_TAP_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_DOUBLE_TAP_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/nest/settings/fragments/navbar/BackLongPress.java
@@ -1,0 +1,25 @@
+package com.nest.settings.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.nest.settings.preferences.AppPicker;
+
+public class BackLongPress extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_LONG_PRESS_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_BACK_LONG_PRESS_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/nest/settings/fragments/navbar/HomeDoubleTap.java
@@ -1,0 +1,25 @@
+package com.nest.settings.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.nest.settings.preferences.AppPicker;
+
+public class HomeDoubleTap extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_HOME_DOUBLE_TAP_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_HOME_DOUBLE_TAP_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}

src/com/nest/settings/fragments/navbar/HomeLongPress.java
@@ -1,0 +1,25 @@
+package com.nest.settings.fragments.navbar;
+
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ListView;
+
+import com.nest.settings.preferences.AppPicker;
+
+public class HomeLongPress extends AppPicker {
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String mAppString = applist.get(position).packageName;
+        String mFriendlyAppString = (String) applist.get(position).loadLabel(packageManager);
+
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_HOME_LONG_PRESS_CUSTOM_APP, mAppString);
+        Settings.System.putString(
+                getContentResolver(), Settings.System.KEY_HOME_LONG_PRESS_CUSTOM_APP_FR_NAME,
+                mFriendlyAppString);
+        finish();
+    }
+}